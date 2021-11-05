6ba775ea40b09cd92ef8d9c7c7532f2d776fa4e1
paulficca1 <paulficca@gmail.com>
2021-11-04 19:06:03 -0500

    added charts functionality to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -14,27 +14,24 @@ import Logo from "../components/logo";
 import { Dimensions } from "react-native";
 import axiosApi from "../data/axios";
 import {
-  ChartDot,
-  ChartPath,
-  ChartPathProvider,
-  monotoneCubicInterpolation,
-  ChartYLabel,
-} from "@rainbow-me/animated-charts";
+  SlideAreaChart,
+  SlideBarChart,
+  SlideBarChartProps,
+  SlideAreaChartProps,
+  YAxisProps,
+  XAxisProps,
+  XAxisLabelAlignment,
+  YAxisLabelAlignment,
+  CursorProps,
+  ToolTipProps,
+  ToolTipTextRenderersInput,
+  GradientProps,
+} from "@connectedcars/react-native-slide-charts";
 
 export const { width: SIZE } = Dimensions.get("window");
 
 export default function Home({ route, navigation }) {
   const [chartData, setChartData] = useState([]);
-
-  const formatUSD = (value) => {
-    "worklet";
-    if (value === "") {
-      return "";
-    }
-    return `$ ${value.toLocaleString("en-US", {
-      currency: "USD",
-    })}`;
-  };
 
   // if (route.params) {
   //   const { coinId } = route.params;
@@ -52,63 +49,72 @@ export default function Home({ route, navigation }) {
       },
     });
     const responseArray = response.data.prices.map((elements) => {
-      return elements[1].toFixed(2);
+      return { x : elements[0], y : elements[1].toFixed(2) } 
     });
     setChartData(responseArray);
     console.log(response.data.prices);
-    console.log(responseArray);
+    console.log(chartData);
   };
 
   useEffect(() => {
     dataFetch();
   }, []);
 
-  const data = [
-    { x: 1453075200, y: 1.47 },
-    { x: 1453161600, y: 1.37 },
-    { x: 1453248000, y: 1.53 },
-    { x: 1453334400, y: 1.54 },
-    { x: 1453420800, y: 1.52 },
-    { x: 1453507200, y: 2.03 },
-    { x: 1453593600, y: 2.1 },
-    { x: 1453680000, y: 2.5 },
-    { x: 1453766400, y: 2.3 },
-    { x: 1453852800, y: 2.42 },
-    { x: 1453939200, y: 2.55 },
-    { x: 1454025600, y: 2.41 },
-    { x: 1454112000, y: 2.43 },
-    { x: 1454198400, y: 2.2 },
-  ];
-
-  const points = monotoneCubicInterpolation({ data, range: 40 });
- 
+  // const data = [
+  //   { x: 1453075200, y: 1.47 },
+  //   { x: 1453161600, y: 1.37 },
+  //   { x: 1453248000, y: 1.53 },
+  //   { x: 1453334400, y: 1.54 },
+  //   { x: 1453420800, y: 1.52 },
+  //   { x: 1453507200, y: 2.03 },
+  //   { x: 1453593600, y: 2.1 },
+  //   { x: 1453680000, y: 2.5 },
+  //   { x: 1453766400, y: 2.3 },
+  //   { x: 1453852800, y: 2.42 },
+  //   { x: 1453939200, y: 2.55 },
+  //   { x: 1454025600, y: 2.41 },
+  //   { x: 1454112000, y: 2.43 },
+  //   { x: 1454198400, y: 2.2 },
+  // ];
 
   return (
-    <ChartPathProvider data={{ points, smoothingStrategy: "bezier" }}>
-      <ImageBackground
-        source={require("../assets/background.jpg")}
-        style={styles.container}
-      >
-        
-          <View style={styles.lowerTitles}>
-            <ChartYLabel
-              format={formatUSD}
-              style={{
-                color: "green",
-                margin: 4,
-                fontSize: 24,
-                fontWeight: "bold",
-              }}
-            />
-          </View>
-          
-        
-        <View style={styles.chartView}>
-        <ChartPath height={SIZE / 2} stroke="yellow" width={SIZE} />
-        <ChartDot style={{ backgroundColor: "blue" }} />
-        </View>
-      </ImageBackground>
-    </ChartPathProvider>
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.container}
+    >
+      <SlideAreaChart
+        data={chartData}
+        chartPaddingTop={100}
+        height={300}
+        scrollable
+        style={{ backgroundColor: 'black'}}
+        shouldCancelWhenOutside={false}
+        axisWidth={12}
+        axisHeight={12}
+        paddingBottom={8}
+        yAxisProps={{
+          verticalLineWidth: 1,
+          axisLabel: 'Price',
+          axisLabelAlignment: 'middle',
+          rotateAxisLabel: true,
+          numberOfTicks: 1,
+          hideMarkers: false,
+        }}
+        xAxisProps={{
+          axisLabel: 'X Units',
+        }}
+        toolTipProps={{
+          toolTipTextRenderers: [
+            ({ scaleY, y }) => ({
+              text: "$" +scaleY
+                .invert(y)
+                .toFixed(2)
+                .toString(),
+            }),
+          ],
+        }}
+      />
+    </ImageBackground>
   );
 }
 
